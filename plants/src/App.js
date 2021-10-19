25a079773cd253691858105051f0a98d9fe1c8b4
Jessica Manak <jessica8angels@gmail.com>
2021-10-18 18:22:18 -0700

    Homepage mostly done

--- plants/src/App.js
+++ plants/src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+import React from 'react'
+import { CssBaseline, AppBar, Toolbar, Typography, Button, Box } from '@mui/material'
+import GrassIcon from '@mui/icons-material/Grass'
+import MainCard from './MainCard';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <CssBaseline />
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <GrassIcon size="large" sx={{ mr: 3 }}/>
+            <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>Water My Plants</Typography>
+            <Button color="secondary" size="medium" variant="outlined">Sign Up Here</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <main>
+        <MainCard />
+      </main>
+    </>
   );
 }
 
---
+++ plants/src/MainCard.js
@@ -0,0 +1,23 @@
+import { Card, CardActionArea, CardContent, CardMedia, Typography } from "@mui/material";
+import React from "react";
+
+export default function MainCard() {
+    return (
+        <Card sx={{ width: 2/2 }}>
+            <CardActionArea>
+                <CardMedia 
+                    component="img"
+                    height="900"
+                    image='https://images.unsplash.com/photo-1555037015-1498966bcd7c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=774&q=80'
+                    title="Plant In Hands"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h3" component="div">Happy Plants, Happy Lives</Typography>
+                    <Typography variant="body1" color="text.secondary">
+                        Taking excellent care of your plants improves the quality of life. It is not always easy to remember to care for your plants. Life gets busy. We understand. That is why we are here to help! Sign up to have detailed instructions on how to care for each of the different species of plants you have in your care. We're here for you!
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+        </Card>
+    );
+}
